2a0e2f46b2a28d59c3e1f22a7fa0b93f364f7ece
Hugo Rambo <hugo.rambo@hotmail.com>
2023-03-10 13:54:28 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './styles/module.styleGlobal.css'
+const App = () => {
+  return (
+   <>
+   <div className={styles.container}>
+
+    teste
+
+   </div>
+   </>
+    
+  )
+}
+
+
+export default App
